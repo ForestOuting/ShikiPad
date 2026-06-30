56e0ac318c9c4bcbeae7eb13229c4e30d0e47a11
ForestOuting <chenbingcheng815@gmail.com>
2026-06-30 13:27:01 +0800

    Restore original mouse trigger logic

--- src/Config.cs
+++ src/Config.cs
@@ -4,13 +4,10 @@ internal sealed class Config {
     public bool Enabled = true;
     public double MouseSensitivity = 1.0;
     public double MouseMaxSpeed = 20.0;
-    public int MouseSlowIntervalMs = 72;
-    public int MouseVelocitySmoothingMs = 32;
     public double RightStickDeadzone = 0.015;
     public string RightStickCurve = "power";
     public double RightStickCurveExponent = 3.0;
     public double MouseScrollCurveExponent = 3.0;
-    public int ScrollVelocitySmoothingMs = 40;
     public double LeftStickEnterDeadzone = 0.35;
     public double LeftStickExitDeadzone = 0.35;
     public double TriggerPressThreshold = 0.25;
